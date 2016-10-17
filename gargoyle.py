d30749870fe20aea6dbfa35b9046a6f603a42aee
Alexandre Pereira <alexandremmartinsp@gmail.com>
2016-10-18 00:28:34 +0100

    save as finished + GUI updated + keyboard keys added

--- gargoyle.py
+++ gargoyle.py
@@ -1,5 +1,6 @@
 from Tkinter import *
 from tkFileDialog import *
+import tkMessageBox
 
 filename = None
 
@@ -16,12 +17,13 @@ def saveFile():
 	f.close()
 
 def saveAs():
-	f = asksaveasfile(mode='w', defaultextension='.txt')
+	f = asksaveasfile(mode='w', defaultextension='.txt',
+		filetypes= [('Text','.txt'), ('HTML', '.html'), ('CSS', '.css'), ('JS', '.js'), ('PHP', '.php'), ('Python', '.py')])
 	t = text.get(0.0, END)
 	try:
 		f.write(t.rstrip())
 	except:
-		showerror(title="Sorry!", message="Unable to save file, please try again...")
+		tkMessageBox.showerror(title="ATTENTION!", message="Unable to save file, please try again.")
 
 def openFile():
 	f = askopenfile(mode='r')
@@ -29,25 +31,24 @@ def openFile():
 	text.delete(0.0, END)
 	text.insert(0.0, t)
 
-
 root = Tk()
 root.title("Gargoyle")
 root.minsize(width=600, height=600)
-root.maxsize(width=700, height=700)
+root.maxsize(width=900, height=600)
 
-text = Text(root, width=700, height=700, font=("Helvetica",16))
+text = Text(root, width=800, height=600, font=('Ubuntu', 16), bg='#282C34', fg='#ffffff', highlightthickness=0)
 text.pack()
 
 menubar = Menu(root)
 filemenu = Menu(menubar)
 menubar.add_cascade(label="File", menu=filemenu)
-filemenu.add_command(label="New", command=newFile)
-filemenu.add_command(label="Open...", command=openFile)
+filemenu.add_command(label="New", command=newFile, accelerator="Cmd+N")
+filemenu.add_command(label="Open...", command=openFile, accelerator="Cmd+O")
 filemenu.add_separator()
-filemenu.add_command(label="Save", command=saveFile)
-filemenu.add_command(label="Save as...", command=saveAs)
+filemenu.add_command(label="Save", command=saveFile, accelerator="Cmd+S")
+filemenu.add_command(label="Save as...", command=saveAs, accelerator="Cmd+Shift+S")
 filemenu.add_separator()
-filemenu.add_command(label="Quit", command=root.quit)
+filemenu.add_command(label="Quit", command=root.quit, accelerator="Cmd+Q")
 
 filemenu = Menu(menubar)
 menubar.add_cascade(label="Help", menu=filemenu)
